93acbd9c8e846d3e5f247399e78546a8097a4e38
yann <yann@raw-labs.com>
2024-06-18 23:23:09 +0200

    Revert "custom tab name"
    This reverts commit 3a6322b765047f6c40a588930418d4d482694d75.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
 import './App.css';
-import AddPlayer from './AddPlayer';
 import PlayersView from './PlayersView';
 import TournamentBracketView from './TournamentBracketView';
 import GenerateTournament from './GenerateTournament';
-import BulkUploadForm from './BulkUploadForm';
 
 function App() {
-  const handleParticipantAdded = () => {
-    console.log('Participants were added');
-  };
-
   return (
     <Router>
-      <Helmet>
-        <title>Open Malley 2024</title>
-      </Helmet>
       <div className="App">
         <header className="App-header">
           <nav className="navigation">
-            <Link to="/addplayer"><button>Add Participant</button></Link>
-            <Link to="/bulk-upload"><button>Upload Participants List</button></Link>
             <Link to="/participants"><button>Participants</button></Link>
-            <Link to="/generate-tournament"><button>Generate</button></Link>
+            <Link to="/generate-tournament"><button>Tournament Brackets</button></Link>
           </nav>
         </header>
         <main className="App-main">
           <Routes>
-            <Route path="/addplayer" element={<AddPlayer />} />
             <Route path="/participants" element={<PlayersView />} />
             <Route path="/generate-tournament" element={<GenerateTournament />} />
             <Route path="/tournament-bracket/:tournamentId" element={<TournamentBracketView />} />
-            <Route path="/bulk-upload" element={<BulkUploadForm onParticipantAdded={handleParticipantAdded} />} />
           </Routes>
         </main>
       </div>
